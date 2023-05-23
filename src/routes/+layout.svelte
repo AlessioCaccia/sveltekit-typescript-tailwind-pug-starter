9cc15bbd9ab439ebd11532db91e619049dd5e032
AlessioCaccia <a.cacciapuoti@tlco.it>
2023-05-23 11:10:16 +0200

    configurazione base

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,12 @@
 </script>
 
 <template lang="pug">
+	//- head
+	svelte:head
+		title Svelte Pug Tailwind Typescript DasyUI Stylus | Starter Template
+		meta(
+			content="A node starter template for the Svelte Preprocessed stack.",
+			name="description"
+		)
 	slot
 </template>
