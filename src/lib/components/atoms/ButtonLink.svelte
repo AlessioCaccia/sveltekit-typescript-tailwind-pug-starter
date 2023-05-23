4471c1ce6dd8115649b7a0310d635940e920f915
AlessioCaccia <a.cacciapuoti@tlco.it>
2023-05-23 18:11:37 +0200

    prova

--- src/lib/components/atoms/ButtonLink.svelte
+++ src/lib/components/atoms/ButtonLink.svelte
@@ -11,7 +11,7 @@ An <a> styled to look like a button.
 </script>
 
 <template lang="pug">
-	.mb-16.w-full(class="sm:mb-24")
+	.mb-14.w-full(class="sm:mb-24")
 		a.text-light.inline-block.rounded.bg-transparent.px-4.py-2.outline.outline-1.outline-white.transition-all(
 			class="hover:bg-accent hover:text-primary hover:outline-transparent",
 			href!="{ targetUrl }",
